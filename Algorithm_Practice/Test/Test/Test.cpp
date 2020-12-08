77c384436864b8ff1951284fd4b1db1a28cd8730
hilariousbanana <hilariousbananana@gmail.com>
2020-12-08 22:24:05 +0900

    120820 저는 알고리즘이 싫어요

--- Algorithm_Practice/Test/Test/Test.cpp
+++ Algorithm_Practice/Test/Test/Test.cpp
@@ -1,22 +1,43 @@
 ﻿#include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class Solution
 {
 public:
-    vector<int> processQueries(vector<int>& queries, int m)
+    int sumOddLengthSubarrays(vector<int>& arr)
     {
-        
-    }
-private:
-    int UpdateList(vector<int>& list, int n)
-    {
-        
+        int result = 0;
+        int turn = (arr.size() + 1) / 2;
+        int length = 1;
+
+        while (turn != 0)
+        {
+            int left = 0, right = length;
+            while (arr.size() - left >= length)
+            {
+                for (int i = left; i < right; i++)
+                {
+                    result += arr[i];
+                    cout << arr[i];
+                }
+                left++;
+                right ++;
+            }
+            cout << endl;
+            length += 2;
+            turn--;
+        }
+
+        return result;
     }
 };
 
 int main()
 {
-   
+    vector<int> temp = { 1, 2, 3 };
+    Solution sol;
+
+    sol.sumOddLengthSubarrays(temp);
 }
