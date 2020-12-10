6d1949f38ec57e1ba75911c816a5cbf700bbbb94
hilariousbanana <hilariousbananana@gmail.com>
2020-12-10 12:46:25 +0900

    121020 Update

--- Algorithm_Practice/Test/Test/Test.cpp
+++ Algorithm_Practice/Test/Test/Test.cpp
@@ -6,38 +6,40 @@ using namespace std;
 class Solution
 {
 public:
-    int sumOddLengthSubarrays(vector<int>& arr)
-    {
-        int result = 0;
-        int turn = (arr.size() + 1) / 2;
-        int length = 1;
+	int countGoodTriplets(vector<int>& arr, int a, int b, int c)
+	{
+		int result = 0;
+		int left = 0, mid = 1, right = 2; //index
 
-        while (turn != 0)
-        {
-            int left = 0, right = length;
-            while (arr.size() - left >= length)
-            {
-                for (int i = left; i < right; i++)
-                {
-                    result += arr[i];
-                    cout << arr[i];
-                }
-                left++;
-                right ++;
-            }
-            cout << endl;
-            length += 2;
-            turn--;
-        }
+		for (int i = left; i < arr.size() - 2; i++)
+		{
+			for (int j = mid; j < arr.size() - 1; j++)
+			{
+				if (abs(arr[i] - arr[j]) <= a)
+				{
+					for (int k = right; k < arr.size(); k++)
+					{
+						if (abs(arr[j] - arr[k]) <= b && abs(arr[i] - arr[k]) <= c)
+						{
+							result++;
+							cout << arr[i] << arr[j] << arr[k] << endl;
+						}
+					}
+				}
+				right++;
+			}
+			mid++;
+		}
 
-        return result;
-    }
+		return result;
+	}
+
 };
 
 int main()
 {
-    vector<int> temp = { 1, 2, 3 };
-    Solution sol;
+	vector<int> temp = { 1, 1, 2, 2, 3 };
+	Solution sol;
 
-    sol.sumOddLengthSubarrays(temp);
+	sol.countGoodTriplets(temp, 0, 0, 1);
 }
